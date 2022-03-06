8405e3440e9066dfa364c8b6ac9eba99159bf152
hemachengalvarayan <hemachengalvarayan@gmail.com>
2022-03-06 11:50:07 +0530

    git jenkis first commit

---
+++ src/test/java/sample1/test1.java
@@ -0,0 +1,37 @@
+package sample1;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class test1 {
+	@Test 
+	public void validateGoogle() throws IOException{
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd = new ChromeDriver();
+		
+		wd.manage().window().maximize();
+		wd.get("https://www.google.co.in/");
+		
+		WebElement element= wd.findElement(By.name("q"));
+		
+		element.sendKeys("Chennai");
+		element.submit();
+		
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("D:/Automation Practice/GitJenkinsTest/src/test/java/sample1/screen/h.png"));	
+		
+}}
